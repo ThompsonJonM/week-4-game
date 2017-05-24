55d9202294af6dc7560d0b90ce4eb66da0992f2e
thompsonjonm <thompson.jonm@gmail.com>
2017-05-24 15:10:59 -0500

    fixed confusing elements in GAME.JS

--- README.md
+++ README.md
@@ -3,6 +3,12 @@ Read from bottom to top.
 
 20170524
 
+GAME.JS: Fixed variables so they are less confusing. Changed from RNG 9-150 to 19-120 based on gitlab rules.
+
+INDEX.HTML: Fixed some grid aspects. 
+
+20170524
+
 GAME.JS: Created variables and added functions. Operates on RNG of 9-150.
 
 INDEX.HTML: Finished preliminary HTML and site structure. Added grid functionality.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,112 +1,113 @@
 $(document).ready(function() {
 
 	//Random number which will be shown in startScore text area
-	//Generates a number between 9 and 150
-	var RNG = Math.floor(Math.random()*141+9)
+	//Generates a number between 19 and 120
+	var rng = Math.floor(Math.random()*101+19)
 
 	//Inserts the number from RNG into startScore area
-	$('#startScore').text(RNG);
+	$('#startScore').text(rng);
 
 	//Gem values
 	//gemOne is Red, gemTwo is Blue, gemThree is Yellow, gemFour is Green
-	//Each gem creates a random number between depending on the gem
-	var gemOne = Math.floor(Math.random()*8+1)
-	var gemTwo = Math.floor(Math.random()*8+1)
-	var gemThree = Math.floor(Math.random()*8+1)
-	var gemFour = Math.floor(Math.random()*8+1)
+	//Each gem creates a random number between 1-12 
+	//This number is then used to increase finalPlayerScore to reach startScore
+	var gemOne = Math.floor(Math.random()*11+1)
+	var gemTwo = Math.floor(Math.random()*11+1)
+	var gemThree = Math.floor(Math.random()*11+1)
+	var gemFour = Math.floor(Math.random()*11+1)
 
 	//Game counts
-	var playerScore = 0;
+	var score = 0;
 	var wins = 0;
 	var losses = 0;
 
-//Prints win and loss counts to their respective text areas
-$('#winCount').text(wins);
-$('#loseCount').text(losses);
+	//Prints win and loss counts to their respective text areas
+	$('#winCount').text(wins);
+	$('#loseCount').text(losses);
 
-//Game reset
-function reset() {
-	RNG = Math.floor(Math.random()*141+9);
-		console.log(RNG)
-		$('#startScore').text(RNG)
-		gemOne = Math.floor(Math.random()*8+1);
-		gemTwo = Math.floor(Math.random()*8+1);
-		gemThree = Math.floor(Math.random()*8+1);
-		gemFour = Math.floor(Math.random()*8+1);
-		playerScore = 0;
-		$('#finalPlayerScore').text(playerScore);
-}
+	//Game reset
+	function reset() {
+		rng = Math.floor(Math.random()*101+19);
+			console.log(rng)
+			$('#startScore').text(rng)
+			gemOne = Math.floor(Math.random()*11+1);
+			gemTwo = Math.floor(Math.random()*11+1);
+			gemThree = Math.floor(Math.random()*11+1);
+			gemFour = Math.floor(Math.random()*11+1);
+			score = 0;
+			$('#finalPlayerScore').text(score);
+	}
 
-//Win alert
-function winText() {
-	alert("You're a crystal master!");
-		wins++;
-		$('#winCount').text(wins);
-		reset();
-}
+	//Win alert
+	function winText() {
+		alert("You're a crystal master!");
+			wins++;
+			$('#winCount').text(wins);
+			reset();
+	}
 
-//Lose alert
-function loseText() {
-	alert("Try Again!");
-		losses++;
-		$('#lossCount').text(losses);
-		reset();
-}
+	//Lose alert
+	function loseText() {
+		alert("Try Again!");
+			losses++;
+			$('#lossCount').text(losses);
+			reset();
+	}
 
-//On-Click functionality for Red
+	//On-Click functionality for Red
 	$('#gemRed').on ('click', function() {
-		playerScore = playerScore + gemOne;
-		console.log("New playerScore= " + playerScore);
+		score = score + gemOne;
+		console.log("New playerScore= " + score);
 
-		$('#finalPlayerScore').text(playerScore);
+		$('#finalPlayerScore').text(score);
 
-			if (playerScore == RNG) {
+			if (score == rng) {
 				winText();
-			} else if (playerScore > RNG) {
+			} else if (score > rng) {
 				loseText();
 			}
 	})
 
-//On-Click functionality for Blue
+	//On-Click functionality for Blue
 	$('#gemBlue').on ('click', function() {
-		playerScore = playerScore + gemTwo;
-		console.log("New playerScore= " + playerScore);
+		score = score + gemTwo;
+		console.log("New playerScore= " + score);
 
-		$('#finalPlayerScore').text(playerScore);
+		$('#finalPlayerScore').text(score);
 
-			if (playerScore == RNG) {
+			if (score == rng) {
 				winText();
 			} 
 
-			else if (playerScore > RNG) {
+			else if (score > rng) {
 				loseText();
 			}
 	})
 
-//On-Click functionality for Yellow
+	//On-Click functionality for Yellow
 	$('#gemYellow').on ('click', function() {
-		playerScore = playerScore + gemThree;
-		console.log("New playerScore= " + playerScore);
+		score = score + gemThree;
+		console.log("New playerScore= " + score);
 
-		$('#finalPlayerScore').text(playerScore);
+		$('#finalPlayerScore').text(score);
 
-			if (playerScore == RNG) {
+			if (score == rng) {
 				winText();
-			} else if (playerScore > RNG) {
+			} else if (score > rng) {
 				loseText();
 			}
 	})
 
-//On-Click functionality for Green
+	//On-Click functionality for Green
 	$('#gemGreen').on ('click', function() {
-		playerScore = playerScore + gemFour;
-		console.log("New playerScore= " + playerScore);
+		score = score + gemFour;
+		console.log("New playerScore= " + score);
 
-		$('#finalPlayerScore').text(playerScore);
+		$('#finalPlayerScore').text(score);
 
-			if (playerScore == RNG) {
+			if (score == rng) {
 				winText();
-			} else if (playerScore > RNG) {
+			} else if (score > rng) {
 				loseText();
 			}
 	});
